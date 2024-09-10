691c41c3e0d15d7e5f2f84e8d91b4a95b5b00eb0
GokulRaj K N <38807518+gokulrajkn@users.noreply.github.com>
2024-09-10 16:15:05 +0530

    Initial Files

---
+++ src/main/java/com/gokulrajkn/httpstreams/HttpstreamsApplication.java
@@ -0,0 +1,13 @@
+package com.gokulrajkn.httpstreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpstreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpstreamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gokulrajkn/httpstreams/HttpstreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokulrajkn.httpstreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpstreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
